d654aad04d8079c5a661f9727905885c4abe5d9b
Nurkulova <cholponnurkulova@gmail.com>
2023-09-29 18:01:30 +0600

    fixed version

--- src/useTheme.js
+++ src/useTheme.js
@@ -1,16 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function useTheme() {
-  const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-  
-  const [theme, setTheme] = useState(prefersDarkMode ? 'dark-mode' : 'light-mode');
+  const savedTheme = localStorage.getItem("theme");
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+
+  const initialTheme =
+    savedTheme || (prefersDarkMode ? "dark-mode" : "light-mode");
+  const [theme, setTheme] = useState(initialTheme);
+
   useEffect(() => {
-    localStorage.setItem('theme', theme);
-    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem("theme", theme);
+    document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
 
   const toggleTheme = () => {
-    const newTheme = theme === 'light-mode' ? 'dark-mode'  : 'light-mode';
+    const newTheme = theme === "light-mode" ? "dark-mode" : "light-mode";
     setTheme(newTheme);
   };
 
